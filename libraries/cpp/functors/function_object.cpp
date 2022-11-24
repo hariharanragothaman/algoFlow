2a25c3567ad7b9b216c5fd38773571706d288ed4
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-11-24 13:31:28 -0500

    What is a functor?

---
+++ libraries/cpp/functors/function_object.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+#include "../../../debug.h"
+using namespace std;
+
+/*
+ *  A function object, or functor, is any type that implements operator().
+ *  This operator is referred to as the call operator or sometimes the application operator.
+ *  The C++ Standard Library uses function objects primarily as sorting criteria for containers and in algorithms.
+ */
+
+class Functor
+{
+public:
+    int operator()(int a, int b)
+    {
+        return a < b;
+    }
+};
+
+int main()
+{
+    Functor f;
+    int a = 5;
+    int b = 7;
+    int ans = f(a, b);
+    cout << ans << endl;
+}
