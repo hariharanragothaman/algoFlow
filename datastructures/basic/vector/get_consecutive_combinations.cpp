28ca5a1f1091e89a43184917f695021a88626125
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-11-24 12:13:35 -0500

    Adding c++ templates for vector

---
+++ datastructures/basic/vector/get_consecutive_combinations.cpp
@@ -0,0 +1,11 @@
+#include "bits/stdc++.h"
+#include "../../../debug.h"
+using namespace std;
+
+int main()
+{
+    vector<int> v1 = {1, 2, 3, 4, 5};
+    for(int i = 0; i < v1.size() -1; i++)
+        cout << v1[i] << v1[i+1] << endl;
+    return 0;
+}
---
+++ datastructures/basic/vector/get_longest_continuous_subsegment.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+#include "../../../debug.h"
+using namespace std;
+
+int get_longest_continuous_segment(string& s, const char& c)
+{
+    int n = s.size();
+    int max_length = 0;
+    int cnt = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        if(s[i] == c)
+            cnt++;
+        else if(s[i] != c)
+            cnt = 0;
+        max_length = max(max_length, cnt);
+    }
+
+    return max_length;
+}
+
+bool checkZeroOnes(string s)
+{
+    return get_longest_continuous_segment(s, '1') > get_longest_continuous_segment(s, '0');
+}
