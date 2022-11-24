b0fd183d1b3ed59763bda983f70090aefa3a1d6f
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-11-24 13:31:16 -0500

    Introduction to threads

---
+++ libraries/cpp/concurrency/create_thread.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+#include "../../../debug.h"
+using namespace std;
+
+void helloFunction()
+{
+    cout << "Hello from a function." << endl;
+}
+
+
+class HelloFunctionObject
+{
+public:
+    void operator()() const
+    {
+        cout << "Hello from a function object." << endl;
+    }
+};
+
+int main()
+{
+    std::cout << std::endl;
+
+    std::thread t1(helloFunction);
+
+    HelloFunctionObject helloFunctionObject;
+    std::thread t2(helloFunctionObject);
+
+    std::thread t3([]{std::cout << "Hello from a lambda." << std::endl;});
+
+    /*
+     * the main thread is waiting until its children are done.
+     */
+    t1.join();
+    t2.join();
+    t3.join();
+
+    std::cout << std::endl;
+
+    /*
+     * The three threads are executed in an arbitrary order; even the three output operations can interleave.
+     * The creator of the child - the main thread in our case - is responsible for the lifetime of the child.
+     */
+
+    return 0;
+}
---
+++ libraries/cpp/concurrency/create_thread_without_join.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+#include "../../../debug.h"
+using namespace std;
+
+/*
+ * The lifetime of a created thread t ends with its callable unit.
+ * Therefore, the creator has two choices:
+ *
+ * It can wait until its child is done: t.join().
+ * It can detach itself from its child: t.detach().
+ *
+ * A t.join() call is useful when the subsequent code relies on the result of the calculation performed in the thread.
+ * t.detach() permits the thread to execute independently from the thread handle t;
+ * therefore, the detached thread will run for the lifetime of the executable.
+ * Typically, we use a detached thread for a long-running background service such as a server.
+ *
+ * A thread t with a callable unit (we can create threads without a callable unit) is called joinable
+ *  if neither t.join() nor a t.detach() is called.
+ *
+ *  The destructor of a joinable thread throws the std::terminate exception;
+ *  this was the reason the program execution terminated with an exception.
+ *
+ *  If we invoke t.join() or t.detach() more than once on a thread t, we get an std::system_error exception.
+ *
+ *  Simply calling t.join() will fix the problem
+ */
+
+int main()
+{
+    std::thread t([]{std::cout << std::this_thread::get_id() << std::endl;});
+}
