cd196b0354790b8e571075055103c27b00012620
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-11-25 11:52:07 -0500

    Adding various thread methods

---
+++ libraries/cpp/concurrency/thread_methods.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+#include "../../../debug.h"
+using namespace std;
+
+
+int main()
+{
+    /*
+     * When the boolalpha format flag is set, bool values are inserted/extracted by their textual representation:
+     * either true or false, instead of integral values.
+     */
+    cout << boolalpha << endl;
+
+    cout << "hardware_concurrency()= "<< thread::hardware_concurrency() << endl;
+
+    thread t1([]{cout << "t1 with id= " << this_thread::get_id() << endl;});
+    thread t2([]{cout << "t2 with id= " << this_thread::get_id() << endl;});
+
+    cout << endl;
+
+    cout << "FROM MAIN: id of t1 " << t1.get_id() << endl;
+    cout << "FROM MAIN: id of t2 " << t2.get_id() << endl;
+
+    cout << endl;
+    swap(t1,t2);
+
+    cout << "FROM MAIN: id of t1 " << t1.get_id() << endl;
+    cout << "FROM MAIN: id of t2 " << t2.get_id() << endl;
+
+    cout << endl;
+
+    cout << "FROM MAIN: id of main= " << this_thread::get_id() << endl;
+
+    cout << endl;
+
+    cout << "t1.joinable(): " << t1.joinable() << endl;
+
+    cout << endl;
+
+    t1.join();
+    t2.join();
+
+    cout << endl;
+
+    cout << "t1.joinable(): " << t1.joinable() << endl;
+
+    cout << endl;
+
+}
