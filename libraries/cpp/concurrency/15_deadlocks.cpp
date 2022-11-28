a72d77f6f0fa6ec34b81a4794290d8969d454389
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-11-27 20:29:42 -0500

    Adding an example of deadlock

---
+++ libraries/cpp/concurrency/15_deadlocks.cpp
@@ -0,0 +1,58 @@
+#include "bits/stdc++.h"
+#include "../../../debug.h"
+using namespace std;
+
+/*
+ * A deadlock is a state in which two or more threads are blocked
+ * because each thread waits for the release of a resource before it releases its resource.
+ */
+
+/*
+ * You can get a deadlock very quickly if you forget to call m.unlock().
+ * That happens for example in the case of an exception in the function getVar().
+ *
+ *      m.lock();
+ *      sharedVar= getVar();
+ *      m.unlock()
+ *
+ *  Locking two mutexes in the wrong order is another typical reason for a deadlock.
+ *  Consider the example below
+ */
+
+struct CriticalData
+{
+    std::mutex mut;
+};
+
+void deadLock(CriticalData& a, CriticalData& b)
+{
+    a.mut.lock();
+    std::cout << "get the first mutex\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+    b.mut.lock();
+    std::cout << "get the second mutex\n";
+
+    a.mut.unlock();
+    b.mut.unlock();
+}
+
+int main()
+{
+    CriticalData c1;
+    CriticalData c2;
+    std::thread t1([&]{ deadLock(c1, c2); });
+    std::thread t2([&]{ deadLock(c2, c1); });
+    t1.join();
+    t2.join();
+}
+
+
+/*
+ *
+ * The short time window of one millisecond (std::this_thread::sleep_for(std::chrono::milliseconds(1)))
+ * is enough to produce with high probability a deadlock
+ * because each thread is waiting forever on the other mutex.
+ * The result is a standstill.
+ *
+ */
