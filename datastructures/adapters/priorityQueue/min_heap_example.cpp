77cddc129d8bed73b9d6f20f53d06e0595fe2583
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-11-29 14:55:30 -0500

    Minium heap example + problem using stack

---
+++ datastructures/adapters/priorityQueue/min_heap_example.cpp
@@ -0,0 +1,66 @@
+#include "bits/stdc++.h"
+#include "../../../debug.h"
+using namespace std;
+/*
+ * By default priority queue in C++ gets the maximum
+ * To get minimum, the following ways can be used.
+ */
+
+void method2()
+{
+    //  Inserting values by changing their sign (using minus (-) for positive number and using plus (+) for negative number :
+    priority_queue<int>pq2;
+    pq2.push(-1); //for +1
+    pq2.push(-2); //for +2
+    pq2.push(-3); //for +3
+    pq2.push(4);  //for -4
+
+    while(!pq2.empty())
+    {
+        int r = pq2.top();
+        pq2.pop();
+        cout<<-r<<" ";
+    }
+
+}
+
+struct compare
+{
+    bool operator()(const int & a, const int & b)
+    {
+        return a>b;
+    }
+};
+
+
+void method3()
+{
+    priority_queue<int,vector<int>,compare> pq;
+    pq.push(1);
+    pq.push(2);
+    pq.push(3);
+
+    while(!pq.empty())
+    {
+        int r = pq.top();
+        pq.pop();
+        cout<<r<<" ";
+    }
+}
+
+
+int main()
+{
+    priority_queue<int,vector<int>,greater<int> >pq;
+    pq.push(1);
+    pq.push(2);
+    pq.push(3);
+
+    while(!pq.empty())
+    {
+        int r = pq.top();
+        pq.pop();
+        cout<<r<< " ";
+    }
+    return 0;
+}
---
+++ testProblems/find_next_greater_element.cpp
@@ -0,0 +1,83 @@
+/*
+    செயல் பேசும் ஆழம் இங்கே சொற்கள் பேசுமா?
+    Focus, Determination and Sheer-Will
+    The woods are lovely, dark and deep,
+    But I have promises to keep,
+    And miles to go before I sleep,
+    And miles to go before I sleep.
+
+    ---------------------------------------------
+    1. Remember the data-type of the I/P's you are getting
+    2. Function calls are not free.
+
+ PROBLEM STATEMENT: https://codeforces.com/group/c3FDl9EUi9/contest/263096/problem/J
+
+*/
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#include "bits/stdc++.h"
+#include <sys/stat.h>
+using namespace std;
+#endif
+
+#define FAST_IO() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define int long long
+#define endl '\n'
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> A(n, 0);
+    vector<int> ans(n, -1);
+    stack<int> S;
+
+    for(int i=0; i<n; i++)
+    {
+        cin >> A[i];
+        while(!S.empty() && A[i] > A[S.top()])
+        {
+            ans[S.top()] = i + 1;
+            S.pop();
+        }
+        S.push(i);
+    }
+
+    // Responding to Queries
+    int q;
+    cin >> q;
+    int index;
+    while(q--)
+    {
+        cin >> index;
+        index = index - 1;
+        cout << ans[index] << endl;
+    }
+}
+
+int32_t main()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    FAST_IO();
+    int T = 1;
+    while(T--)
+    {
+        solve();
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+#ifndef ONLINE_JUDGE
+    if(fileExists("data.in"))
+    {
+        cout << "Time taken: ";
+        cout << duration.count() << endl;
+    }
+    return 0;
+#else
+#endif
+}
