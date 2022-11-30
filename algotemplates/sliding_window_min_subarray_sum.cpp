3ae4f4c971c93094f18ef8faa311028d60e958cd
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-11-29 21:14:04 -0500

    Adding template for minimum subarray sum

---
+++ algotemplates/sliding_window_min_subarray_sum.cpp
@@ -0,0 +1,33 @@
+#include "../debug.h"
+using namespace std;
+
+void minSubArraySum(vector<int> A, int n, int m)
+{
+    int left = 0;
+    int right = 0;
+    int csum = 0;
+    int ans = INT_MAX;
+
+    while (right < n)
+    {
+        csum += A[right];
+
+        while( csum >= m && left <= right)
+        {
+            ans  = min(ans, right-left+1);
+            csum -= A[left];
+            left += 1;
+        }
+
+        right += 1;
+    }
+    if(ans != INT_MAX)
+        cout << ans << endl;
+    else
+        cout << -1 << endl;
+}
+
+int main()
+{
+    return 0;
+}
