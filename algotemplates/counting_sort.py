55df0380d8252ea81179a31c6f53d282adf26ec9
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2023-08-18 12:30:56 -0400

    Updates to counting sort algorithm

--- algotemplates/counting_sort.py
+++ algotemplates/counting_sort.py
@@ -1,28 +1,32 @@
 def counting_sort(A):
+    """
+    Current limitation works only for 0 to max(A)
+    Needs update for negative numbers
+    """
     mx = max(A)
     n = len(A)
     cnt = [0] * (mx+1)
-    output = [0] * (mx+1)
+    output = [0] * n
 
     # Building the count array
     for c in A:
         cnt[c] += 1
-    print("The cnt is:", cnt)
+    # print("The cnt is:", cnt)
 
     num_items_before = 0
     for i, counts in enumerate(cnt):
         cnt[i] = num_items_before
         num_items_before += counts
-    print("The cnt is:", cnt)
+    # print("The cnt is:", cnt)
 
     for c in A:
         output[cnt[c]] = c
         cnt[c] += 1
+        # print(f"The output is:", output)
     return output
 
 
 if __name__ == '__main__':
-    A = [1, 4, 1, 2, 7, 5, 2]
-    print(A)
+    A = [7, 7, 6, 1, 1, 2, 3, 4, 7]
     result = counting_sort(A)
     print(result)
