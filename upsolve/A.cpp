5e5b937588f750edc8e1cb0f34decae692c9f414
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2023-09-23 21:27:35 -0400

    Syning folder from codesetup

---
+++ upsolve/A.cpp
@@ -0,0 +1,100 @@
+/*
+    செயல் பேசும் ஆழம் இங்கே சொற்கள் பேசுமா?
+    Focus, Determination and Sheer-Will
+    The woods are lovely, dark and deep,
+    But I have promises to keep,
+    And miles to go before I sleep,
+    And miles to go before I sleep.
+
+    ---------------------------------------------
+    1. Remember the data-type of the I/P's you are getting
+    2. Function calls are not free.
+
+*/
+
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#include "bits/stdc++.h"
+#include <sys/stat.h>
+using namespace std;
+#endif
+
+#define FAST_IO() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define int long long
+#define endl '\n'
+
+/*
+ *  FUNCTIONS FOR DEBUGGING
+ */
+
+inline bool fileExists (const std::string& name)
+{
+    struct stat buffer;
+    return (stat (name.c_str(), &buffer) == 0);
+}
+
+template<typename T>
+void debug(const T& msg)
+{
+    if(fileExists("data.in"))
+        cout << msg << endl;
+}
+
+void debug2()
+{
+    if(fileExists("data.in"))
+        cout << "---------------------------------" << endl;
+}
+
+void debug3()
+{
+    if(fileExists("data.in"))
+        cout << "*********************************" << endl;
+}
+
+template<typename T>
+void print(const T &v)
+{
+    if(fileExists("data.in"))
+    {
+        for(auto c: v)
+            cout << c << " ";
+        cout << endl;
+    }
+}
+
+
+
+void solve()
+{
+
+}
+
+int32_t main()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    FAST_IO();
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        solve();
+        debug2();
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+#ifndef ONLINE_JUDGE
+    if(fileExists("data.in"))
+    {
+        cout << "Time taken: ";
+        cout << duration.count() << endl;
+    }
+    return 0;
+#else
+#endif
+}
