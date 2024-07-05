c31957622058bb7945e698a83a553f96014d1519
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-07-05 12:50:34 -0400

    LeetCode 206: Reverse LinkedList

---
+++ problems/leetcode/reverse-linked-list.h
@@ -0,0 +1,36 @@
+#include "../../debug.h"
+
+
+/**
+ * Definition for singly-linked list.
+ */
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        ListNode* current = head;
+
+        while(current)
+        {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+};
