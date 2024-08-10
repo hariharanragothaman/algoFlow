1e22391b4aaa4bab281dda78abf713950013f092
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2024-08-10 04:25:39 -0400

    Threads Condition Variable.

--- libraries/cpp/threads/threads_condition_variable.cpp
+++ libraries/cpp/threads/threads_condition_variable.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 mutex m;
 condition_variable cv;
-std::string data;
+std::string output;
 bool ready = false;
 bool processed = false;
 
@@ -15,18 +15,14 @@ bool processed = false;
 void worker_fn()
 {
     unique_lock lk(m);
-
     // wait until main() sends data.
     cv.wait(lk, []{return ready; });
-
     // after the wait, we own the lock
     cout << "Worker thread is processing data" << endl;
-    data += " After Processing..." ;
-
+    output += " After Processing..." ;
     // Send data back to main()
     processed = true;
     cout << "Worker thread processing is completed" << endl;
-
     lk.unlock();
     cv.notify_one();
 }
@@ -34,21 +30,20 @@ void worker_fn()
 int main()
 {
     thread t1(worker_fn);
-    data = "Data from Main";
-
+    output = "Data from Main";
     // Send data to worker thread
     {
         lock_guard lk(m);
         ready = true;
         cout << "main() signals data ready for processing" << endl;
     }
-    cv.notify_one();
 
+    cv.notify_one();
     // wait for worker
     {
         unique_lock lk(m);
         cv.wait(lk, [] {return processed;});
     }
-    cout << "Back in main(), data = " << data << endl;
+    cout << "Back in main(), data = " << output << endl;
     t1.join();
 }
