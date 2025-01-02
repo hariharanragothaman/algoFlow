e4b81920a529b00db859d332fef39cfa3935ecb3
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2025-01-01 23:10:20 -0500

    Adding Prefix sum template

---
+++ algoFlow/algo/prefixSum/prefixSum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+vector<int> prefixSum(vector<int> A)
+{
+    int n = A.size();
+    vector<int> PS(n+1, 0);
+    for(int i=1; i< PS.size(); i++)
+    {
+        PS[i] = PS[i-1] + A[i-1];
+    }
+    return A
+}
+
+int main()
+{
+    vector<int> T = {1, 2, 3, 4, 5};
+    vector<int> PS_T = prefixSum(T);
+    // We are assuming zero-indexing, hence to find sum between start and end
+    int start  = 2, end = 4;
+    int ans = PS_T[end+1] - PS_T[start];
+    cout << "The answer is: " << ans << endl;
+}
